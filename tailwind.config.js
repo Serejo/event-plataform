c155abb5c59dd4f6e878bb071b58bb64743835ac
Erick Serejo <erickrainier@hotmail.com>
2022-06-25 21:47:20 -0300

    aula 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage:{
+       blur:'url(/src/img/blur.png)'   
+      },
       fontFamily:{
         sans: "Roboto, sans-serif",
       },
